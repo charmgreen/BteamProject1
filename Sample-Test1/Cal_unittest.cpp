aabced40dc1a1325232d05e320fa252352b022ce
JJY <binaryfalse81@gmail.com>
2024-07-02 16:16:36 +0900

    sum, sumsum 구현 완료

--- Sample-Test1/Cal_unittest.cpp
+++ Sample-Test1/Cal_unittest.cpp
@@ -1,7 +1,18 @@
 #include "pch.h"
 #include "../getWhat/Cal.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseCal, TestSum)
+{
+  Cal c;
+  int sum = c.getSum(1, 1);
+
+  EXPECT_EQ(2, sum);
+}
+
+TEST(TestCaseCal, TestSumSum)
+{
+    Cal c;
+    int sum = c.getSumSum(1, 1, 1);
+
+    EXPECT_EQ(3, sum);
 }
--- getWhat/Cal.cpp
+++ getWhat/Cal.cpp
@@ -1,5 +1,12 @@
 class Cal
 {
 public:
-
+    int getSum(int a, int b)
+    {
+        return a + b;
+    }
+    int getSumSum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
 };
