2b2f6527130e45f4e7349602bb4a27dadc91885f
minq <james2224@hanmail.net>
2024-07-02 16:28:00 +0900

    getDivide function, unit test 추가

--- Sample-Test1/Cal_unittest.cpp
+++ Sample-Test1/Cal_unittest.cpp
@@ -5,3 +5,35 @@ TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+
+TEST(TsCalcDivide, TcCalcDivide000) {
+
+	Cal c;
+	EXPECT_EQ(c.getDivide(100, 10), 10);
+}
+
+TEST(TsCalcDivide, TcCalcDivide001) {
+
+	Cal c;
+	EXPECT_EQ(c.getDivide(1, 1), 1);
+}
+
+TEST(TsCalcDivide, TcCalcDivide002) {
+
+	Cal c;
+	EXPECT_EQ(c.getDivide(0, 1), 0);
+}
+
+TEST(TsCalcDivide, TcCalcDivide003) {
+
+	Cal c;
+	// Check Divide By Zero 회피
+	EXPECT_EQ(c.getDivide(100, 0), 2147483647);
+}
+
+TEST(TsCalcDivide, TcCalcDivide004) {
+
+	Cal c;
+	EXPECT_EQ(c.getDivide(-100, 0), -2147483648);
+}
--- getWhat/Cal.cpp
+++ getWhat/Cal.cpp
@@ -1,5 +1,14 @@
+
 class Cal
 {
 public:
-
+	int getDivide(int a, int b)
+	{
+		if (b == 0)
+		{
+			// Divide by Zero 회피를 위해 int max, min으로 return.
+			return (a >= 0) ? ((int)2147483647) : ((int)-2147483648);
+		}
+		return (a / b);
+	}
 };
